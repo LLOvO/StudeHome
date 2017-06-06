157fb4cd869a2f88bd9b63280b311583ad118174
Leon <404683592@qq.com>
2017-06-06 10:30:26 +0800

    更新readme

--- app/src/main/java/com/liangquan/studyhome/home/StudyHomeActivity.java
+++ app/src/main/java/com/liangquan/studyhome/home/StudyHomeActivity.java
@@ -1,16 +1,13 @@
 package com.liangquan.studyhome.home;
 
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.util.SparseArrayCompat;
-import android.util.Log;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -18,10 +15,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.liangquan.studyhome.R;
 import com.liangquan.studyhome.home.fragment.StudyHomeFragment;
 import com.liangquan.studyhome.home.fragment.UIFragment;
-import com.liangquan.studyhome.R;
 import com.liangquan.studyhome.home.fragment.WorkerFragment;
 
 public class StudyHomeActivity extends AppCompatActivity
